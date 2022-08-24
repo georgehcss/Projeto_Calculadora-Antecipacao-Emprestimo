10a7b21c0208062c78a93b3adff29d44b18cacde
GeorgeHCSS <george.henriquess@hotmail.com>
2022-08-24 18:29:20 +0200

    Edição do Span

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const calcParcelaAntecipada = () => {
 if (parcelaAntecipada.value !== '' && valorDaParcela.value !== '' && taxa.value !== '' && parcelasPagas.value !== ''){
     const antecipacaoEmprestimo = (valorDaParcela.value/ (1 + (taxa.value / 100))**(parcelaAntecipada.value - parcelasPagas.value)).toFixed(2)
     
-    resultado.innerHTML = `O valor da Parcela Antecipada é de R$  ${antecipacaoEmprestimo}`
+    resultado.innerHTML = `O valor da Parcela Antecipada é de: <br><br>
+    R$  ${antecipacaoEmprestimo}`
 }
     else{
         resultado.innerHTML = 'Preencha os Campos' 
