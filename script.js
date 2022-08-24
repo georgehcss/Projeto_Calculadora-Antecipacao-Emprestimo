b496c12be320bf34c7e30698c97d5553028328f6
GeorgeHCSS <george.henriquess@hotmail.com>
2022-08-24 17:53:52 +0200

    Deixando Responsivo

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ const taxa = document.querySelector("#taxa");
 const parcelaAntecipada = document.querySelector("#parcelaAntecipada");
 const resultado = document.querySelector("#resultado");
 
-
 const calcParcelaAntecipada = () => {
 if (parcelaAntecipada.value !== '' && valorDaParcela.value !== '' && taxa.value !== '' && parcelasPagas.value !== ''){
     const antecipacaoEmprestimo = (valorDaParcela.value/ (1 + (taxa.value / 100))**(parcelaAntecipada.value - parcelasPagas.value)).toFixed(2)
-    
     
     resultado.innerHTML = `O valor da Parcela Antecipada é de R$  ${antecipacaoEmprestimo}`
 }
