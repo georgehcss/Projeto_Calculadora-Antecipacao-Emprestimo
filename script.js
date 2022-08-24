d1a08def6995358cbd78fe114bf022ddde40e654
GeorgeHCSS <george.henriquess@hotmail.com>
2022-08-24 15:45:37 +0200

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const parcelasPagas = document.querySelector("#parcelasPagas");
+const valorDaParcela = document.querySelector("#valorDaParcela");
+const taxa = document.querySelector("#taxa");
+const parcelaAntecipada = document.querySelector("#parcelaAntecipada");
+const resultado = document.querySelector("#resultado");
+
+//const antecipacaoEmprestimo = (valorDaParcela.value / (1 + taxa.value)**(parcelaAntecipada.value - parcelasPagas)).toFixed(2)
+
+
+const calcParcelaAntecipada = () => {
+if (parcelaAntecipada.value !== '' && valorDaParcela.value !== '' && taxa.value !== '' && parcelasPagas.value !== ''){
+    const antecipacaoEmprestimo = (valorDaParcela.value/ (1 + (taxa.value / 100))**(parcelaAntecipada.value - parcelasPagas.value)).toFixed(2)
+    
+    
+    resultado.innerHTML = `O valor da sua Parcela pagando de forma Antecipada é de R$  ${antecipacaoEmprestimo}`
+}
+    else{
+        resultado.innerHTML = 'Preencha os Campos' 
+    }
+}
